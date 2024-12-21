5bcb0b5266e152d451c7dfe1434c5c5ed5f7e08a
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-21 23:46:55 +0530

    create basic express server

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+//This File is made so that all middleware will be applied here
+// and index file doesn't get congested 
+import express from "express";
+
+
+
+
+const app = express()
+
+
+
+
+
+
+
+export default app
---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+import app from "./app.js";
+
+dotenv.config({
+    path : "./.env"
+})
+
+
+
+app.listen(process.env.PORT , ()=>{
+    console.log("server ok and listening on : " , process.env.PORT)
+})
+
