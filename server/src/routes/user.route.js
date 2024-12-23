11a0c3c90e1f70a2e161c23ab28f56126e749e54
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-23 22:58:28 +0530

    `Added secure routes and updated user controller imports in user.route.js`

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,6 +1,12 @@
 import express from "express"
 import {upload} from '../middlewares/multer.middleware.js'
-import { handleUserRegistration } from "../controllers/user.controller.js"
+import { handleUserLogin, 
+    handleUserRegistration ,
+    handleChangePassword ,   
+    handleUpdateUserProfile , 
+    handleUserLogout ,
+} from "../controllers/user.controller.js"
+import verifyJwt from "../middlewares/auth.middleware.js"
 
 
 const router = express.Router()
@@ -10,6 +16,22 @@ router.route("/register").post(
     upload.single("avatar"),
     handleUserRegistration
 )
+
+
+//Secure Routes
+router.route("/login").post(verifyJwt , handleUserLogin)
+router.route("/logout").post(verifyJwt , handleUserLogout)
+router.route("/update-profile").patch(verifyJwt , handleUpdateUserProfile)
+router.route("/change-password").patch(verifyJwt , handleChangePassword)
+// router.route("/refresh-token").post(handleRefreshToken)
+// router.route("/get-user-profile").get(handleGetUserProfile)
+// router.route("/update-avatar").patch(handleUpdateAvatar)
+// router.route("/get-all-users").get(handleGetAllUsers)
+// router.route("/get-user-by-id").get(handleGetUserById)
+// router.route("/delete-user").delete(handleDeleteUser)
+
+
+router.route("/login").get(handleUserLogin)
 
 
 
