93b326ad58441f6cc73003cabbc03a9cf4d8edeb
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-26 23:55:12 +0530

    minor update in auth function

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,36 +1,41 @@
 import { ApiError } from "../utils/ApiError.util.js"
 import jwt from 'jsonwebtoken'
-import {User} from '../models/user.model.js'
+import { User } from '../models/user.model.js'
+import { ApiResponse } from "../utils/ApiResponse.util.js"
 
 
 
 
-const verifyJwt = async(req , res ,  next)=>{
+const verifyJwt = async (req, res, next) => {
    try {
-     const token  =  req.cookies?.accessToken || req.headers("Authorization").split(" ")[1]
- 
-     if(!token){
-        throw new ApiError(400 , "Access Token not Present")
-     }
+      const token = req.cookies?.accessToken || req.headers["Authorization"]?.split(" ")[1]
 
-     const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
-     console.log(decodedToken)
+      if (!token) {
+         throw new ApiError(400, "Access Token not Present : Please Login to get Token")
+      }
 
-     if(!decodedToken){
-        throw new ApiError(400 , "Invalid Access Token")
-     }
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+      console.log(decodedToken)
 
-     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+      if (!decodedToken) {
+         throw new ApiError(400, "Invalid Access Token")
+      }
 
-     if(!user){
-        throw new ApiError(400 , "Invalid Token  : Not able to find user with token")
-     }
-     
+      const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
 
-     req.user = user
-     next()
- }catch (error) {
-    throw new ApiError(400 , `Unauthorized : ${error.message}`)
+      if (!user) {
+         throw new ApiError(400, "Invalid Token  : Not able to find user with token")
+      }
+
+
+      req.user = user
+      next()
+   } catch (error) {
+      res
+         .status(400)
+         .json(
+            new ApiResponse(400, `Unauthorized : ${error.message}`)
+         )
    }
 }
 
