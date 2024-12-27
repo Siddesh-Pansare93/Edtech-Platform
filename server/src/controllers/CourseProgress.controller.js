95e490310ebf9fc01d51fc1b24615ee645b12315
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-27 18:11:14 +0530

    add controller to get update course progress

---
+++ server/src/controllers/CourseProgress.controller.js
@@ -0,0 +1,58 @@
+import { CourseProgress } from "../models/courseProgress.model";
+import { ApiResponse } from "../utils/ApiResponse.util";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+const updateCourseProgress = asyncHandler(async (req, res) => {
+    //get courseid -- userId doc from db 
+    //check lessonId is there in completed videos array
+    //if there return response already completed
+    //else add lesson id to completed videos array in doc 
+
+    try {
+
+        const { courseId, lessonId } = req.params
+        const userId = req.user._id
+
+        //Finding if the course Progress Doc id ther or not 
+        const courseProgress = await CourseProgress.findOne({
+            user: userId,
+            course: courseId
+        })
+
+        if (!courseProgress) {
+            return ApiResponse(res, 404, "Course Progress Not Found")
+        }
+
+        //Checking if the user had already completed the lesson or not
+        const isLessonCompleted = courseProgress.completedVideos.includes(lessonId)
+        if (isLessonCompleted) {
+            return ApiResponse(res, 200, "Lesson Already Completed")
+        }
+
+        //If not already completed ... add lesson Id to completed videos array
+        courseProgress.completedVideos.push(lessonId)
+
+        //saving the updated doc in db 
+        await courseProgress.save()
+
+
+        res
+            .status(200)
+            .json(
+                new ApiResponse(res, 200, "Lesson Completed Successfully")
+            )
+    } catch (error) {
+        res
+            .status(error.statusCode || 400)
+            .json(
+                new ApiResponse(error.statusCode || 400, null, `ERROR : Updating Course Progress : ${error.message}`)
+            )
+    }
+})
+
+
+
+export { 
+    updateCourseProgress
+}
