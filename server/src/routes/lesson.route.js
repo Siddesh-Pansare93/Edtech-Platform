4799fc3b142a4082d4fa55efd040e369b7b4fb1c
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-27 13:25:30 +0530

    `Add Api endPoints for lesson`

---
+++ server/src/routes/lesson.route.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import verifyJwt from '../middlewares/auth.middleware.js'
+import { handleAddLesson, handleDeleteLesson, handleUpdateLesson } from '../controllers/lesson.controller.js'
+import { isInstructorOrAdmin } from '../middlewares/role.middleware'
+
+const router  = express.Router()
+
+router.use(verifyJwt)
+router.use(isInstructorOrAdmin)
+
+router.route("/:courseId/:sectionId/add")
+    .post(handleAddLesson)
+
+router.route("/:courseId/:sectionId/:lessonId")
+    .patch(handleUpdateLesson)
+    .delete(handleDeleteLesson)
