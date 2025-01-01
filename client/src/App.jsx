445e0463608a900aae469da51ce101cbe4ec116a
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-01 23:59:33 +0530

    update App component to include a button with submit handler and a greeting message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Payment from './components/Payment'
 import Verify from './components/verify'
+import { Button } from './components/ui/button';
 
 
 function App() {
+
+  const handleSubmit = ()=>{
+    console.log("submit")
+  }
   return (
-   
-    
-    <Routes>
-      <Route path="/payment" element={<Payment />} />
-      <Route path="/verify" element={<Verify />} />
-   
-    </Routes>
+    <>
+      <h1>HIi From Siddesh</h1>
+      <Button onClick={handleSubmit}>
+        Click me
+      </Button>
+    </>
   )
 }
 
