1403183ea56340a223744f99c643bf8d80832899
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-02 23:47:03 +0530

    integrate Redux and Redux Persist for state management and update routing for CourseDetails page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,14 +8,20 @@ import Home from './components/Home';
 import Login from './components/core/Login';
 import SignUp from './components/core/SignUp';
 import { ThemeProvider } from './components/Common/ThemeProvider';
+import { Provider } from 'react-redux';
+import { store , persistor} from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import HomeAfterLogin from './components/HomeAfterLogin';
+import CourseDetails from './Pages/CourseDetails';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
 
-    <Route path="/" element={<Layout/>}>
-      <Route path="/home" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<SignUp />}/>
+    <Route path="/" element={<Layout />}>
+      <Route path="/home" element={<HomeAfterLogin />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/course-details/:id" element={<CourseDetails />} />
     </Route>
 
   )
@@ -26,7 +32,11 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
 
-  <ThemeProvider>
-  <RouterProvider router={router} />
-  </ThemeProvider>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>
 )
