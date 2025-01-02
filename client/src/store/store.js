8740919fdadf5acc9d7bea4f7bb85b38aac13e2a
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-02 23:48:19 +0530

    implement Redux Persist for state persistence in the store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Features/authSlice";
+import { persistStore , persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    auth : authReducer
+})
+
+
+const persistConfig = {
+    key : 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
 
 const store = configureStore({
-
-    reducer: {
-        auth : authReducer
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+          },
+        }),
 });
+
+let persistor = persistStore(store)
+
+export  {store, persistor};
