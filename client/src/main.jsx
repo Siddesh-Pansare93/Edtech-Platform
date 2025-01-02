aad02979228e9777cc981f5f3af0957a5d22c0e3
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-02 18:26:33 +0530

    refactor routing setup to use createBrowserRouter with new routes for Home, Login, and SignUp components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './components/Home';
+import Login from './components/core/Login';
+import SignUp from './components/core/SignUp';
+import { ThemeProvider } from './components/Common/ThemeProvider';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+    <Route path="/" element={<Layout/>}>
+      <Route path="/home" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<SignUp />}/>
+    </Route>
+
+  )
+)
 
 
 
 
 createRoot(document.getElementById('root')).render(
 
-  
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-    
-  
+  <ThemeProvider>
+  <RouterProvider router={router} />
+  </ThemeProvider>
 )
