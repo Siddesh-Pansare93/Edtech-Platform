d7c901ea55c93f9888c8ea4d5ffe56c2e2f5b50c
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-02 23:48:30 +0530

    add CourseDetails component to fetch and display course information

---
+++ client/src/Pages/CourseDetails.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect ,  useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axiosInstance from '@/utils/axiosInstance'
+
+
+const CourseDetails = () => {
+    const { id } = useParams()
+    const [courseDetails, setCourseDetails] = useState({})
+
+    useEffect(() => {
+        console.log(id)
+        
+        const fetchCourseDetails = async () => {
+            try {
+                const response = await axiosInstance.get(`/course/details/${id}`)
+                console.log(response)
+                if (response.data.success) {
+                    setCourseDetails(response.data.data)
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        }
+
+        fetchCourseDetails()
+
+    }  , [id])
+  return (
+    <div>
+        <h1>Course Details</h1>
+        <p>Course ID: {id}</p>
+        <div>
+        <p>Title: {courseDetails.title}</p>
+        <p>Price: {courseDetails.price}</p>
+        <p>{courseDetails.description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default CourseDetails
