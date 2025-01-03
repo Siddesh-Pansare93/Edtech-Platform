06d9d81462d4f0cd14f8f9b30f310404188d9eab
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-03 23:50:10 +0530

    style(HeroSection): update layout and image styling for improved responsiveness and visual appeal

--- client/src/components/Common/HomePage/HeroSection.jsx
+++ client/src/components/Common/HomePage/HeroSection.jsx
@@ -2,13 +2,13 @@
 
 import { motion } from 'framer-motion'
 import { ArrowRight } from 'lucide-react'
-import herobg from '../../../assets/herobg.svg'
+
 
 const Hero = () => {
   return (
     <section className="bg-gradient-to-b from-blue-100 to-white dark:from-gray-900 dark:to-gray-800 py-20 ">
       <div className="container mx-auto px-10 pt-32 ">
-        <div className="flex flex-col md:flex-row items-center md:justify-between ">
+        <div className="flex flex-col md:flex-row items-center md:justify-evenly ">
           <motion.div
             className="md:w-1/2 mb-10 md:mb-0"
             initial={{ opacity: 0, x: -50 }}
@@ -31,15 +31,15 @@ const Hero = () => {
             </motion.button>
           </motion.div>
           <motion.div
-            className="md:w-1/3"
+            className="md:w-1/4"
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
           >
             <img
-              src={herobg}
+              src="https://img.freepik.com/free-vector/hand-coding-concept-illustration_114360-8413.jpg?t=st=1735915072~exp=1735918672~hmac=4c652d9685233d2403bffcccbfe2923d3361f16d8167d67b89242da746589740&w=740"
               alt="Students learning online"
-              className="rounded-lg shadow-2xl"
+              className="rounded-[50%] shadow-2xl"
             />
           </motion.div>
         </div>
