7934a1e9a5c825c527f4aa62c44f5e6775ff8ac0
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-03 20:05:12 +0530

    add Home component: implement main page layout with sections for hero, features, popular courses, testimonials, and call to action

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import HeroSection from '@/components/Common/HomePage/HeroSection'
+import WhyChooseUs from '@/components/Common/HomePage/WhyChooseUs'
+import PopularCourses from '@/components/Common/HomePage/PopularCourses'
+import Testimonials from '@/components/Common/HomePage/Testimonials'
+import HowItWorks from '@/components/Common/HomePage/HowItWorks'
+import CallToAction from '@/components/Common/HomePage/CallToAction'
+import Header from '@/components/Common/Header'
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <WhyChooseUs />
+      <PopularCourses />
+      <Testimonials />
+      <HowItWorks />
+      <CallToAction />
+    </>
+  )
+}
+
