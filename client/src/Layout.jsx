61abd0538d5fc760a4f1f91381033ec5a132109d
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-03 20:02:12 +0530

    add Footer component

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,13 +1,18 @@
-import React, { Children } from 'react'
+import React, { Children, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import  Header  from './components/Common/Header'
+import Login from './components/core/Login'
+import Footer from './components/Common/Footer'
 
 function Layout() {
+  const [isLoginOpen , setIsLoginOpen] = useState(false)
+  console.log(isLoginOpen)
+
   return (
     <>
     <Header/>
     <Outlet/>
-
+    <Footer/>
     </>
   )
 }
