6e2557eeca696ad06e9b02ea4a4ed0baed1c07ec
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-03 23:50:16 +0530

    style(Login): enhance layout and animations for improved user experience

--- client/src/components/core/Login.jsx
+++ client/src/components/core/Login.jsx
@@ -8,6 +8,7 @@ import { login } from '@/store/Features/authSlice'
 import { motion, AnimatePresence } from 'framer-motion'
 
 
+
 function Login({ isOpen, onClose }) {
     const [submitting, setSubmitting] = useState(false)
     const { register, handleSubmit, formState: { errors } } = useForm()
@@ -31,25 +32,27 @@ function Login({ isOpen, onClose }) {
 
     return (
         <AnimatePresence>
-            {isOpen && (
+            (
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className={` h-screen  grid grid-cols-1 md:grid-cols-2  place-items-center z-50 bg-gradient-to-r from-blue-300 to-gray-300 dark:from-gray-900 dark:to-gray-800 `}
+                onClick={onClose}
+            >
                 <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-                    onClick={onClose}
+                    initial={{ scale: 0.9, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    exit={{ scale: 0.9, opacity: 0 }}
+                    transition={{ type: 'spring', damping: 15 }}
+                    className="bg-transparent rounded-lg p-8 max-w-md w-full col-span-1"
+                    onClick={(e) => e.stopPropagation()}
                 >
-                    <motion.div
-                        initial={{ scale: 0.9, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        exit={{ scale: 0.9, opacity: 0 }}
-                        transition={{ type: 'spring', damping: 15 }}
-                        className="bg-white rounded-lg p-8 max-w-md w-full"
-                        onClick={(e) => e.stopPropagation()}
-                    >
-                        <div className="flex justify-between items-center mb-6">
-                            <h2 className="text-2xl font-bold">Login</h2>
-                            <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+                    <div className="flex flex-col justify-center mb-6">
+                        <h2 className="text-3xl font-bold text-black dark:text-white mb-5">Welcome Back</h2>
+                        
+                        <p>Build skills for today, tomorrow, and beyond.<span className='text-black dark:text-yellow-300 italic font-semibold'> Education to future-proof your career. </span></p>
+                        {/* <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
@@ -64,55 +67,67 @@ function Login({ isOpen, onClose }) {
                                         d="M6 18L18 6M6 6l12 12"
                                     />
                                 </svg>
-                            </button>
-                        </div>
-                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 flex flex-col items-center">
-                            {/* Email Input */}
-                            {/* Username Input */}
-                            <Input
-                                name="username"
-                                placeholder="Username"
-                                type="text"
-                                label="Username"
-                                className={``}
-                                {...register('username', { required: 'Username is required' })}
-                            />
-                            {errors.username && <p className="text-red-500">{errors.username.message}</p>}
-                            <Input
-                                name="email"
-                                placeholder="Enter your email"
-                                type="email"
-                                label="Email"
-                                {...register('email', { required: 'Email is required' })}
-                                className="w-full"
-                            />
-                            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+                            </button> */}
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 flex flex-col items-center">
+                        {/* Email Input */}
+                        {/* Username Input */}
+                        <Input
+                            name="username"
+                            placeholder="Username"
+                            type="text"
+                            label="Username"
+                            className={``}
+                            {...register('username', { required: 'Username is required' })}
+                        />
+                        {errors.username && <p className="text-red-500">{errors.username.message}</p>}
+                        <Input
+                            name="email"
+                            placeholder="Enter your email"
+                            type="email"
+                            label="Email"
+                            {...register('email', { required: 'Email is required' })}
+                            className="w-full"
+                        />
+                        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
 
-                            {/* Password Input */}
-                            <Input
-                                name="password"
-                                placeholder="Enter your password"
-                                type="password"
-                                label="Password"
-                                {...register('password', {
-                                    required: 'Password is required',
-                                })}
-                                className="w-full"
-                            />
-                            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+                        {/* Password Input */}
+                        <Input
+                            name="password"
+                            placeholder="Enter your password"
+                            type="password"
+                            label="Password"
+                            {...register('password', {
+                                required: 'Password is required',
+                            })}
+                            className="w-full"
+                        />
+                        {errors.password && <p className="text-red-500">{errors.password.message}</p>}
 
-                            <motion.button
-                                type="submit"
-                                whileHover={{ scale: 1.05 }}
-                                whileTap={{ scale: 0.95 }}
-                                className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition-colors"
-                            >
-                                {submitting ? "Submitting..." : "Login"}
-                            </motion.button>
-                        </form>
-                    </motion.div>
+                        <motion.button
+                            type="submit"
+                            whileHover={{ scale: 1.05 }}
+                            whileTap={{ scale: 0.95 }}
+                            className="w-full  bg-blue-600 dark:bg-yellow-500 dark:text-black font-semibold text-white py-2 rounded-md hover:bg-blue-700 transition-colors"
+                        >
+                            {submitting ? "Submitting..." : "Login"}
+                        </motion.button>
+                    </form>
                 </motion.div>
-            )}
+                <motion.div
+                    className="md:w-1/2 col-span-1"
+                    initial={{ opacity: 0, x: 50 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5, delay: 0.2 }}
+                >
+                    <img
+                        src="https://img.freepik.com/free-vector/hand-coding-concept-illustration_114360-8413.jpg?t=st=1735915072~exp=1735918672~hmac=4c652d9685233d2403bffcccbfe2923d3361f16d8167d67b89242da746589740&w=740"
+                        alt="Students learning online"
+                        className="rounded-xl shadow-2xl"
+                    />
+                </motion.div>
+            </motion.div>
+            )
         </AnimatePresence>
     )
 }
