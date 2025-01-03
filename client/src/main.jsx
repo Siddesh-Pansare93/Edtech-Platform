85652e32e5023d604d5e69ba0f99170e961f31bd
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-03 20:02:39 +0530

    refactor routing: update Home component import and adjust routes in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Layout from './Layout';
-import Home from './components/Home';
+import Home from './Pages/Home';
 import Login from './components/core/Login';
 import SignUp from './components/core/SignUp';
 import { ThemeProvider } from './components/Common/ThemeProvider';
@@ -14,13 +14,17 @@ import { PersistGate } from 'redux-persist/integration/react';
 import HomeAfterLogin from './components/HomeAfterLogin';
 import CourseDetails from './Pages/CourseDetails';
 
+
+
+
+
 const router = createBrowserRouter(
   createRoutesFromElements(
 
     <Route path="/" element={<Layout />}>
-      <Route path="/home" element={<HomeAfterLogin />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login/>} />
+      {/* <Route path="/signup" element={<SignUp />} /> */}
       <Route path="/course-details/:id" element={<CourseDetails />} />
     </Route>
 
