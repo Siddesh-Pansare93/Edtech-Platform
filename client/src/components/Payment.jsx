85203a28aede4402aa47d7613907c3f4d673896c
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-05 23:48:17 +0530

    feat(Payment): update courses array to use dynamic ID from URL parameters

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 
 import '../App.css'
+import { useParams } from 'react-router-dom'
 
 function Payment() {
 
+  const {id} = useParams()
       
-  const courses = ["676c5623a7d76ccf7a555c8d" , "6770080868aec54fcf74dee1" ]
+  const courses = [id]
 
   const runFetch = async ()=>{
     const response = await fetch('http://localhost:8000/api/v1/enroll/purchase' , {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json', 
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzY5ODcyMzBmMWJlZWU5NjdiMWUxNDMiLCJ1c2VybmFtZSI6InNpZGRlc2g5MyIsImVtYWlsIjoic2lkZGVzaHBhbnNhcmU5M0BnbWFpbC5jb20iLCJyb2xlIjoiaW5zdHJ1Y3RvciIsImlhdCI6MTczNTM4OTI1MCwiZXhwIjoxNzM1NDc1NjUwfQ.-HEnKt1329cTiNldxFP3_hL2wrkAjCZ0fegI_Kcx4Hs'
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzY5ODcyMzBmMWJlZWU5NjdiMWUxNDMiLCJ1c2VybmFtZSI6InNpZGRlc2g5MyIsImVtYWlsIjoic2lkZGVzaHBhbnNhcmU5M0BnbWFpbC5jb20iLCJyb2xlIjoiaW5zdHJ1Y3RvciIsImlhdCI6MTczNjA5MzIxMCwiZXhwIjoxNzM2MTc5NjEwfQ.2hHfNacfW--LObEKx3DQZwULjZGflcLbLCi6eV-miwo'
         },
         body: JSON.stringify({
           courses
           })
     })
     const data = await response.json()
+    console.log(data)
     
     if(data.success){
       window.location.replace(data.data)
