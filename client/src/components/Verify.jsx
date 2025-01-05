46d0b3461f549e4debdfc304da8f1ec023e31b16
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-05 23:50:49 +0530

    feat(Verify): replace fetch with axios for payment verification

--- client/src/components/Verify.jsx
+++ client/src/components/Verify.jsx
@@ -1,3 +1,4 @@
+import axiosInstance from '@/utils/axiosInstance';
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 
@@ -12,19 +13,14 @@ function Verify() {
     console.log(courseIds)
 
     const onClick = async () => {
-        const verificationResponse = await fetch("http://localhost:8000/api/v1/users/verify-payment", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzY5ODcyMzBmMWJlZWU5NjdiMWUxNDMiLCJ1c2VybmFtZSI6InNpZGRlc2g5MyIsImVtYWlsIjoic2lkZGVzaHBhbnNhcmU5M0BnbWFpbC5jb20iLCJyb2xlIjoiaW5zdHJ1Y3RvciIsImlhdCI6MTczNTM4OTI1MCwiZXhwIjoxNzM1NDc1NjUwfQ.-HEnKt1329cTiNldxFP3_hL2wrkAjCZ0fegI_Kcx4Hs'
-            },
-            body : JSON.stringify({
-                success,
-                courseIds
-            })
-            })
-            const data = await verificationResponse.json()
-            console.log(data)
+        const response = axiosInstance.post("/users/verify-payment" , {
+            success , 
+            courseIds
+        })
+
+        console.log(response.data.data)
+
+
             
     }
     return (
