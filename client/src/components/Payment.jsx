a4ef9965bbe0deba2cf77db243f90890df53b7fe
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-07 01:02:29 +0530

    feat(Payment): refactor payment handling to use axios and improve navigation on success

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -2,29 +2,30 @@ import { useState } from 'react'
 
 import '../App.css'
 import { useParams } from 'react-router-dom'
+import axiosInstance from '@/utils/axiosInstance'
+import { useNavigate } from 'react-router-dom'
 
 function Payment() {
 
+  
   const {id} = useParams()
-      
   const courses = [id]
 
+  const navigate = useNavigate()
+
   const runFetch = async ()=>{
-    const response = await fetch('http://localhost:8000/api/v1/enroll/purchase' , {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', 
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzY5ODcyMzBmMWJlZWU5NjdiMWUxNDMiLCJ1c2VybmFtZSI6InNpZGRlc2g5MyIsImVtYWlsIjoic2lkZGVzaHBhbnNhcmU5M0BnbWFpbC5jb20iLCJyb2xlIjoiaW5zdHJ1Y3RvciIsImlhdCI6MTczNjA5MzIxMCwiZXhwIjoxNzM2MTc5NjEwfQ.2hHfNacfW--LObEKx3DQZwULjZGflcLbLCi6eV-miwo'
-        },
-        body: JSON.stringify({
-          courses
-          })
-    })
-    const data = await response.json()
+    console.log(courses)
+    const response = await axiosInstance.post("/enroll/purchase" , {courses})
+    console.log(response)
+    const data = await response.data
     console.log(data)
     
     if(data.success){
-      window.location.replace(data.data)
+      alert("Payment Successful")
+      if(data.data === null ){
+        navigate(`/course-details/${courses[0]}`)
+      }
+      // window.location.replace(data.data)
     }else{
       alert(data.message)
     }
