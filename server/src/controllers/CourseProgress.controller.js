c340bf88919a1e3cc420bfb8a4d044327e434442
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-12 23:49:37 +0530

    feat(CourseProgress): enhance course progress tracking with lesson completion logic

--- server/src/controllers/CourseProgress.controller.js
+++ server/src/controllers/CourseProgress.controller.js
@@ -3,6 +3,7 @@ import { ApiResponse } from "../utils/ApiResponse.util.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Course } from "../models/course.model.js";
 import { ApiError } from "../utils/ApiError.util.js"
+import mongoose from "mongoose";
 
 
 const updateCourseProgress = asyncHandler(async (req, res) => {
@@ -22,30 +23,48 @@ const updateCourseProgress = asyncHandler(async (req, res) => {
             course: courseId
         })
 
+        // If course Progress doesn't exist ..... create one and directly add lesson Id into it 
         if (!courseProgress) {
-            throw new ApiError(400, "Course Progress not found")
+            const courseProgressCreation = await CourseProgress.create({
+                user: userId,
+                course: courseId,
+                completedVideos: [lessonId]
+            })
 
-            //Checking if the user had already completed the lesson or not
-            const isLessonCompleted = courseProgress.completedVideos.includes(lessonId)
-            if (isLessonCompleted) {
-                return ApiResponse(res, 200, "Lesson Already Completed")
-            }
-
-            //If not already completed ... add lesson Id to completed videos array
-            courseProgress.completedVideos.push(lessonId)
-
-            //saving the updated doc in db 
-            await courseProgress.save()
-
-
-            res
+            return res
                 .status(200)
                 .json(
-                    new ApiResponse(res, 200, "Lesson Completed Successfully")
+                    new ApiResponse(200, courseProgressCreation, "Lesson Completed Successfully")
                 )
         }
+
+        //Checking if the user had already completed the lesson or not
+        const isLessonCompleted = courseProgress?.completedVideos.includes(lessonId)
+        if (isLessonCompleted) {
+            console.log("inside each ...")
+            return res
+                .status(200)
+                .json(
+                    new ApiResponse(200, null, "Lesson Already Completed"))
+        }
+
+        //If not already completed ... add lesson Id to completed videos array
+        courseProgress.completedVideos.push(lessonId)
+
+
+
+        //saving the updated doc in db 
+        await courseProgress.save()
+
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, courseProgress, "Lesson Completed Successfully")
+            )
+
     } catch (error) {
-        res
+        return res
             .status(error.statusCode || 400)
             .json(
                 new ApiResponse(error.statusCode || 400, null, `ERROR : Updating Course Progress : ${error.message}`)
@@ -63,16 +82,75 @@ const getCourseProgress = asyncHandler(async (req, res) => {
             user: userId,
             course: courseId
         })
+
+
         if (!courseProgress) {
-            throw new ApiError(400, "Course Progress Not found")
+            return res
+                .status(200)
+                .json(
+                    new ApiResponse(200, { progress: 0 }, "Course Progress fetched Successfully")
+                )
         }
 
-        const course = Course.findById(courseId)
-        const TotalLessons = course.lessons.length()
-        const completedLessons = courseProgress.completedVideos.length()
-        const progress = (completedLessons / TotalLessons) * 100
 
-        res
+
+        const totalLessonsInCourse = await Course.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(courseId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "sections",
+                    localField: "sections",
+                    foreignField: "_id",
+                    as: "sections",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "lessons",
+                                localField: "lessons",
+                                foreignField: "_id",
+                                as: "lessons"
+                            }
+                        },
+                        {
+                            $addFields: {
+                                totalLessons: { $size: "$lessons" }
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    totalLessons: {
+                        $sum: {
+                            $map: {
+                                input: "$sections",
+                                as: "section",
+                                in: "$$section.totalLessons"
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    totalLessons: 1
+                }
+            }
+
+        ])
+
+        const totalLessons = totalLessonsInCourse[0].totalLessons
+
+
+        const completedLessons = courseProgress?.completedVideos?.length
+        const progress = (completedLessons / totalLessons) * 100
+
+        return res
             .status(200)
             .json(
                 new ApiResponse(200, progress, "Course Progress fetched Successfully")
@@ -80,7 +158,7 @@ const getCourseProgress = asyncHandler(async (req, res) => {
 
 
     } catch (error) {
-        res
+        return res
             .status(error.statusCode || 400)
             .json(
                 new ApiResponse(error.statusCode || 400, null, `ERROR : Getting Course Progress : ${error.message}`)
@@ -90,6 +168,6 @@ const getCourseProgress = asyncHandler(async (req, res) => {
 })
 
 export {
-    updateCourseProgress , 
+    updateCourseProgress,
     getCourseProgress
 }
