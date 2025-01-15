89b78dce302f2de4bf7cbcf81c6f5adf622895a7
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-01-16 01:13:51 +0530

    feat(Logo): add link to home in the logo component

--- client/src/components/Common/Logo.jsx
+++ client/src/components/Common/Logo.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
     return (
         <div className="logo">
+            <Link to={"/home"}>
             <h1 className="text-xl font-bold text-black dark:text-teal-400">SKILL VULTURES</h1>
+            </Link>
         </div>
     );
 };
